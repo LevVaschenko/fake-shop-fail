0dbcafbbe20b906829b5b7068923f8ac4eb8e080
imac <levvaschenko@gmail.com>
2022-10-22 17:53:53 -0700

    added list

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react"
 
+// with jsx
+const title = <h1>Hello React.js</h1>
+
+const list = (
+    <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+    </ul>
+)
+
+// without jsx
+// const title = React.createElement('h1', null, 'React')
+
 function App() {
-  return <div className="App"></div>
+    return (
+        <div className="App">
+            {title} {list} {10+5}
+        </div> )
 }
 
 export default App
