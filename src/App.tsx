90b618913e29f0f7bca5281828d5475491ecc3b2
imac <levvaschenko@gmail.com>
2022-10-22 18:33:32 -0700

    added React.Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -20,12 +20,17 @@ const list = (
 const Title = () => <h1>Hello React Component</h1>
 
 const Title1 = () =>  {
-    return (   
-        <ul>
-            <li>123</li>
-            <li>456</li>
-            <li>789</li>
-        </ul>
+    let a = 0;
+    return (
+        <React.Fragment>
+           <ul>
+                <li>{a + 1} 123</li>
+                <li>456</li>
+                <li>789</li>
+            </ul>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi possimus error blanditiis repellendus impedit laborum beatae dignissimos doloribus excepturi fugit soluta praesentium architecto, quo ducimus perspiciatis veritatis corrupti sint voluptatibus.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Modi possimus error blanditiis repellendus impedit laborum beatae dignissimos doloribus excepturi fugit soluta praesentium architecto, quo ducimus perspiciatis veritatis corrupti sint voluptatibus.</p>
+        </React.Fragment>
     )
 }
     
@@ -35,7 +40,7 @@ function App() {
         <div className="App">
             <Title/>
             <Title1/>
-            {list} {10+5}
+            {list}
         </div> )
 }
 
