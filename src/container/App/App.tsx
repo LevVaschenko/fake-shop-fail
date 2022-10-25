54cade6282b26580a16c95a896eab15fbfe4dda1
imac <levvaschenko@gmail.com>
2022-10-24 22:46:35 -0700

    add app bar and container

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react' /* здесь я прописал tsrafce */
-import CssBaseline from '@mui/material/CssBaseline';
-import Header from './Header/Header'
-import Main from './Main/Main'
-import Footer from './Footer/Footer'
+import CssBaseline from '@mui/material/CssBaseline'
+import Header from 'container/Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
 
 type Props = {}
 
